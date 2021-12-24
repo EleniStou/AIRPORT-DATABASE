ebf3a907e58c17524e29d5a6dbee59bb88501024
EleniStou <96616573+EleniStou@users.noreply.github.com>
2021-12-24 12:33:17 +0200

    Add files via upload

--- airport.sql
+++ airport.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
-
+--ΠΤΗΣΗ
 DROP TABLE IF EXISTS "FLIGHT";
 CREATE TABLE IF NOT EXISTS "FLIGHT"(
     'id_flight' integer NOT NULL
@@ -10,14 +10,14 @@ CREATE TABLE IF NOT EXISTS "FLIGHT"(
     CONSTRAINT "flight_airplane_FK" FOREIGN KEY ('id_airplane') REFERENCES 'AIRPLANE' ('id_airplane') ON DELETE CASCADE ON UPDATE CASCADE
     CONSTRAINT "flight_line_FK" FOREIGN KEY ('id_line') REFERENCES 'LINE' ('id_line') ON DELETE CASCADE ON UPDATE CASCADE
 );
-
+--ΓΡΑΜΜΗ
 DROP TABLE IF EXISTS "LINE";
 CREATE TABLE IF NOT EXISTS "LINE"(
     'id_line' integer NOT NULL
     'date_line' datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
     PRIMARY KEY ('id_line')
  );
-
+--ΑΕΡΟΠΛΑΝΟ
  DROP TABLE IF EXISTS "AIRPLANE";
  CREATE TABLE IF NOT EXISTS "AIRPLANE"(
     'id_airplane' integer NOT NULL
@@ -30,3 +30,11 @@ CREATE TABLE IF NOT EXISTS "LINE"(
     CONSTRAINT "airplane_category_FK" FOREIGN KEY ('name_category') REFERENCES 'CATEGORY' ('name_category') ON DELETE CASCADE ON UPDATE CASCADE
     CONSTRAINT "airplane_airline_FK" FOREIGN KEY ('id_airline') REFERENCES 'AIRLINE' ('id_airline') ON DELETE CASCADE ON UPDATE CASCADE
  );
+--ΚΑΤΗΓΟΡΙΑ
+ DROP TABLE IF EXISTS "CATEGORY";
+ CREATE TABLE IF NOT EXISTS "CATEGORY"(
+     'name_category' varchar(30) NOT NULL
+    PRIMARY KEY('name_category')
+ );
+
+ 
