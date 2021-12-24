0b045c588db51aac37a204d0e4d0cff88dc305da
GlTheodorou <93772403+GlTheodorou@users.noreply.github.com>
2021-12-24 15:37:59 +0200

    Add files via upload

--- airport.sql
+++ airport.sql
@@ -99,3 +99,35 @@ CREATE TABLE IF NOT EXISTS "AIRLINE"(
  ('Az','ITA Airways','BookingsChangesRefunds@itaspa.com','ΙΤΑΛΙΑ','ΡΩΜΗ','www.itaspa.com','8003618336'),
  ('TK','Turkish Airlines','sales.muc@thy.com','ΤΟΥΡΚΙΑ','ΚΩΝΣΤΑΝΤΙΝΟΥΠΟΛΗ','www.turkishairlines.com','6986799849');
 
+-- ΠΡΟΟΡΙΣΜΟΣ 
+DROP TABLE IF EXISTS "DESTINATION";
+ CREATE TABLE IF NOT EXISTS "DESTINATION"(
+    'id_aerodromio_destination' INTEGER NOT NULL,
+    'id_grammi_destination' INTEGER NOT NULL,
+    'ID_destination' INTEGER NOT NULL,
+    PRIMARY KEY('ID_destination')
+    CONSTRAINT "id_aerodromio_destination_FK" FOREIGN KEY ('id_aerodromio_destination') REFERENCES 'DESTINATION' ('id_aerodromio_destination') ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "id_grammi_destination_FK" FOREIGN KEY ('id_grammi_destination') REFERENCES 'DESTINATION' ('id_grammi_destination') ON DELETE CASCADE ON UPDATE CASCADE
+ ); 
+
+ --ΑΦΙΞΗ 
+ DROP TABLE IF EXISTS "DEPARTURE";
+ CREATE TABLE IF NOT EXISTS "DEPARTURE"(
+     'id_aerodromio_departure' INTEGER NOT NULL,
+     'id_grammi_departure' INTEGER NOT NULL,
+     'ID_departure' INTEGER NOT NULL,
+    PRIMARY KEY('ID_departure')
+    CONSTRAINT "id_aerodromio_departure_FK" FOREIGN KEY ('id_aerodromio_departure') REFERENCES 'DEPARTURE' ('id_aerodromio_depsrture') ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "id_grammi_departure_FK" FOREIGN KEY ('id_grammi_departure') REFERENCES 'DEPARTURE' ('id_grammi_despsrture') ON DELETE CASCADE ON UPDATE CASCADE
+ );
+ 
+ --ΕΝΔΙΑΜΕΣΟΣ ΣΤΑΘΜΟΣ
+  DROP TABLE IF EXISTS "MID";
+ CREATE TABLE IF NOT EXISTS "MID"(
+     'id_aerodromio_mid' INTEGER NOT NULL,
+     'id_grammi_mid' INTEGER NOT NULL,
+     'ID_mid' INTEGER NOT NULL,
+    PRIMARY KEY('ID_mid')
+    CONSTRAINT "id_aerodromio_mid_FK" FOREIGN KEY ('id_aerodromio_mid') REFERENCES 'MID' ('id_aerodromio_mid') ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "id_grammi_mid_FK" FOREIGN KEY ('id_grammi_mid') REFERENCES 'MID' ('id_grammi_mid') ON DELETE CASCADE ON UPDATE CASCADE
+ )
